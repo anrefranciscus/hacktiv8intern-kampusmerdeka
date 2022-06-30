8df0c8032b6d6bbcabe9e84e5f815cb3fe711437
Anre Franciscus <anrefranciscus@gmail.com>
2022-07-01 01:56:08 +0700

    assignment-7

---
+++ SESI-16/ASSIGNMENT-7/kanban_apps/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Kanban from "./pages/Kanban";
+
+function App() {
+  return (
+    <div className='flex h-screen bg-gray-200'>
+      <div className='m-10'>
+        <Kanban />
+      </div>
+    </div>
+  );
+}
+
+export default App;
